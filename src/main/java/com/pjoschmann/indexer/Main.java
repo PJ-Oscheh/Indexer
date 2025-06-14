3a2798c8fc4f932bd85c8aa04d5c3a847a9d00d6
PJ Oschmann <phillip236.po@gmail.com>
2025-06-14 13:56:49 -0400

    Save site map after crawl

--- src/main/java/com/pjoschmann/indexer/Main.java
+++ src/main/java/com/pjoschmann/indexer/Main.java
@@ -1,16 +1,16 @@
 package com.pjoschmann.indexer;
 
 import java.io.IOException;
+import java.util.HashMap;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         WebCrawler wc = new WebCrawler();
 
         try {
-            wc.crawl("https://ezbatteryreconditioning.com", "https://ezbatteryreconditioning.com");
+            HashMap<String, SiteInfo> siteMap = wc.crawl("https://ezbatteryreconditioning.com",
+                    "https://ezbatteryreconditioning.com");
         }
         catch (IOException e) {
             System.out.println("Failed to traverse site: " + e.getLocalizedMessage());
