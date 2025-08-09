ebe581fd9bdc8fc884afa06c13ffd22d955b4227
PJ Oschmann <phillip236.po@gmail.com>
2025-08-09 15:20:59 -0400

    Compiles now horray

--- src/main/java/com/pjoschmann/indexer/Main.java
+++ src/main/java/com/pjoschmann/indexer/Main.java
@@ -1,14 +1,22 @@
 package com.pjoschmann.indexer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+@RestController
+@SpringBootApplication
 public class Main {
 
-
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
